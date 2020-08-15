0572b9fa291637cffe9a68455af398627c22582e
Amanda Neal <68088863+amandaneal1131@users.noreply.github.com>
2020-08-15 14:14:17 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,36 @@
+var questions = [{
+        title: 'What is Javascript?',
+        choices: ['words', 'coding language ', 'coding math', 'Florida'],
+        answer: 'coding language',
+    },
+
+    {
+        title: 'How would you write a console?',
+        choices: ['console.log', 'console(log)', 'console=log', 'console.console'],
+        answer: 'console.log',
+    },
+
+    {
+        title: 'What is a commonly used site in javascript?',
+        choices: ['JavaBava', 'Cowboy Boots', 'Bootstrap', 'Puss n Boots'],
+        answer: 'Bootstrap',
+    },
+
+    {
+        title: 'What are the main componenet in VS Code that we have used in class so far?',
+        choices: ['C#,C++,python', 'C++,js,css', 'python,html,js', 'html,css,js'],
+        answer: 'html,css,js',
+    },
+
+    {
+        title: 'WIll this ever get easier?',
+        choices: ['Yes', 'Nah', 'Nope', 'No'],
+        answer: 'Yes',
+    },
+
+    {
+        title: 'What is a main component written in code?',
+        choices: ['container', 'containersssss', 'your majesty', 'containerdog'],
+        answer: 'container',
+    },
+];
